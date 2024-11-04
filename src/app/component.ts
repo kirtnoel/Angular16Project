8161a3278af580d02af12aebddf806f78fa23577
Kirt Noel <42869984+kirtnoel@users.noreply.github.com>
2024-11-04 13:37:58 -0500

    listing 11.2

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,7 +1,6 @@
-import { Component, computed, effect, signal } from "@angular/core";
+import { Component, computed } from "@angular/core";
 import { Model } from "./repository.model";
-import { Ticker } from "./ticker.model";
-import { toSignal } from "@angular/core/rxjs-interop";
+import { Product } from "./product.model";
 
 @Component({
     selector: "app",
@@ -9,44 +8,11 @@ import { toSignal } from "@angular/core/rxjs-interop";
 })
 export class ProductComponent {
     private model: Model = new Model();
-    private messages = ["Total", "Price"];
-    private index = signal<number>(0);
-    private ticker = new Ticker();
-    tickerValue = toSignal(this.ticker.value, { initialValue: 0 });
 
-    // constructor() {
-    //     this.ticker.value.subscribe(newValue =>
-    //         this.tickerValue = newValue);
-    // }
+    products = computed<Product[]>(() => this.model.Products());
 
-    count = computed<number>(() => this.model.Products().length);
+    count = computed<number>(() => this.products().length);
 
-    countEffect = effect(() =>
-        console.log(`count value computed: ${this.count()}`));
-
-    get total(): string {
-        let result = this.model.Products()
-            .reduce((total, p) => total + (p.price ?? 0), 0).toFixed(2);
-        console.log(`total value read: ${result}`);
-        return result;
-    }
-
-    message = computed<string>(() =>
-        `${this.messages[this.index()]} $${this.total} `
-            + ` Ticker: ${this.tickerValue()}`);
-
-    messageEffect = effect(() =>
-        console.log(`message value computed: ${this.message()},`));
-
-    toggleMessage() {
-        console.clear();
-        console.log("toggleMessage method invoked");
-        this.index.update(currentVal => (currentVal + 1) % 2);
-    }
-
-    removeProduct() {
-        console.clear();
-        console.log("removeProduct method invoked");
-        this.model.deleteProduct(this.model.Products()[0].id ?? 0);
-    }
+    classes = computed<string>(() =>
+        this.count() == 5 ? "bg-success" : "bg-warning");
 }
