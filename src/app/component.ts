5702f1bda995e1406723830876ee1267178ee5bc
Kirt Noel <42869984+kirtnoel@users.noreply.github.com>
2024-11-06 11:07:13 +0000

    listing 12.2

--- src/app/component.ts
+++ src/app/component.ts
@@ -9,34 +9,17 @@ import { Product } from "./product.model";
 export class ProductComponent {
     private model: Model = new Model();
  
+    targetName: string = "Kayak";
+ 
     products = computed<Product[]>(() => this.model.Products());
  
     count = computed<number>(() => this.products().length);
  
-    classes = computed<string>(() => 
-        this.count() == 5 ? "bg-success" : "bg-warning");
- 
-    getClasses(key: number) {
-        return "p-2 " + (((this.products()[key].price ?? 0) > 50) 
-            ? "bg-info" : "bg-warning");
-    }        
- 
-    getClassMap(key: number): Object {
-        let product = this.products()[key];
-        return {
-            "text-center bg-danger": product.name == "Kayak",
-            "bg-info": (product.price ?? 0) < 50
-        };
+    product(key: number): Product | undefined {
+        return this.model.getProduct(key);
     }
  
-    fontSizeWithUnits: string = "30px";
-    fontSizeWithoutUnits: string= "30";
- 
-    getStyles(key: number) {
-        return {
-            fontSize: "30px",
-            "margin.px": 100,
-            color: (this.products()[key].price ?? 0) > 50 ? "red" : "green"
-        };
+    removeProduct() {
+        this.model.deleteProduct(this.model.Products()[0].id ?? 0);
     }
 }
