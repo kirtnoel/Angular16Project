ec399b9f19cb1d5857e5a6b81b0e84e1a1aa71e7
Kirt Noel <42869984+kirtnoel@users.noreply.github.com>
2024-11-06 10:41:04 +0000

    listing 11.16

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,26 +1,26 @@
 import { Component, computed } from "@angular/core";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
-
+ 
 @Component({
     selector: "app",
     templateUrl: "template.html"
 })
 export class ProductComponent {
     private model: Model = new Model();
-
+ 
     products = computed<Product[]>(() => this.model.Products());
-
+ 
     count = computed<number>(() => this.products().length);
-
-    classes = computed<string>(() =>
+ 
+    classes = computed<string>(() => 
         this.count() == 5 ? "bg-success" : "bg-warning");
-
+ 
     getClasses(key: number) {
-        return "p-2 " + (((this.products()[key].price ?? 0) > 50)
+        return "p-2 " + (((this.products()[key].price ?? 0) > 50) 
             ? "bg-info" : "bg-warning");
-    }
-
+    }        
+ 
     getClassMap(key: number): Object {
         let product = this.products()[key];
         return {
@@ -28,4 +28,7 @@ export class ProductComponent {
             "bg-info": (product.price ?? 0) < 50
         };
     }
+ 
+    fontSizeWithUnits: string = "30px";
+    fontSizeWithoutUnits: string= "30";
 }
