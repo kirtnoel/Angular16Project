4e26d6a861ece7c188ff99ed1fd498a9a423112c
Kirt Noel <42869984+kirtnoel@users.noreply.github.com>
2024-11-07 05:32:27 -0500

    listing 12.10

--- src/app/component.ts
+++ src/app/component.ts
@@ -1,25 +1,33 @@
 import { Component, computed } from "@angular/core";
 import { Model } from "./repository.model";
 import { Product } from "./product.model";
- 
+
 @Component({
     selector: "app",
     templateUrl: "template.html"
 })
 export class ProductComponent {
     private model: Model = new Model();
- 
+
     targetName: string = "Kayak";
- 
+
     products = computed<Product[]>(() => this.model.Products());
- 
+
     count = computed<number>(() => this.products().length);
- 
+
     product(key: number): Product | undefined {
         return this.model.getProduct(key);
     }
- 
+
     removeProduct() {
         this.model.deleteProduct(this.model.Products()[0].id ?? 0);
+    }
+
+    swapProduct() {
+        let p = this.products()[0];
+        if (p != null && p.id != null) {
+            this.model.deleteProduct(p.id);
+            this.model.saveProduct( { ...p, id: 0 });
+        }
     }
 }
